171be9d9edf35e684d160ad3497819c659f13399
Wahyu Santos <wahyusantoso1997@gmail.com>
2019-02-13 12:36:56 +0700

    hidden menu

--- application/views/Homepage_V.php
+++ application/views/Homepage_V.php
@@ -179,20 +179,20 @@ Author:   Webstrot
                                     </div>
                                 </div>
                             </div>
-                            <div class="sw_icon_bar">
+                            <!--<div class="sw_icon_bar">
                                 <ul class="tc_login_btn_wrapper">
                                     <li class="dropdown tc_login_btn">
                                         <a class="dropdown-toggle hvr-float-shadow active waves-effect waves-light" data-toggle="dropdown" href="#"><i class="fa fa-ellipsis-v"></i></a>
                                         <ul class="dropdown-menu tc_menu_fixed_border">
-                                            <!-- <li class="dropdown_hover"><a href="#">FAQ's</a></li>
-                                            <li class="dropdown_hover"><a href="#">Track Order</a></li> -->
+                                            <a!-- <li class="dropdown_hover"><a href="#">FAQ's</a></li>
+                                            <li class="dropdown_hover"><a href="#">Track Order</a></li> --a>
                                             <li class="dropdown_hover"><a href="#">Register</a></li>
                                             <li class="dropdown_hover"><a href="#">Login</a></li>
                                             <li class="dropdown_hover hidden-lg"><a href="#">Join Us</a></li>
                                         </ul>
                                     </li>
                                 </ul>
-                            </div>
+                            </div>-->
 
                         </div>
 
@@ -200,12 +200,12 @@ Author:   Webstrot
                 </div>
                 <!-- /.main-menu-wrapper -->
                 <!-- mobile menu area start -->
-                <div class="mobile-menu-area visible-sm visible-xs">
+                <!--<div class="mobile-menu-area visible-sm visible-xs">
                     <div class="container header_container">
                         <div class="row">
 
                             <div class="col-xs-12 cc_menu_top_margin">
-                                <!-- mobile menu start -->
+                                <a!-- mobile menu start -a->
                                 <div class="mobile-menu">
                                     <nav>
                                         <ul class="nav">
@@ -310,7 +310,7 @@ Author:   Webstrot
 
                                         </ul>
                                     </nav>
-                                </div>
+                                </div>-->
                                 <!-- mobile menu end -->
                             </div>
                         </div>
