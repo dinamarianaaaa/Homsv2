e19e156f0cf5e59a75785f8e65e53eedb95242d2
kiddowin <officiallyanakayah@gmail.com>
2019-03-04 19:31:45 +0700

    register login view

--- application/controllers/Login_C.php
+++ application/controllers/Login_C.php
@@ -1,10 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Register_C extends CI_Controller {
+class Login_C extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('Register_V');
+		$this->load->view('Login_V');
 	}
 }
