57dbd0307b62dfbcc4a54fb66f0d4b5c2373fac5
kiddowin <officiallyanakayah@gmail.com>
2019-03-16 11:13:39 +0700

    add view dashboard 2

--- application/controllers/Dashboard1_C.php
+++ application/controllers/Dashboard1_C.php
@@ -1,11 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Dashboard_C extends CI_Controller {
+class Dashboard1_C extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('Dashboard_V');
+		$this->load->view('Dashboard1_V');
 		
 	}
 }
