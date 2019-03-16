5d0bb9a6f08d3b6dbc422d5d805b4a16bb0fbbb9
Nasywai <nasywaibtsmh@gmail.com>
2019-03-16 10:45:57 +0700

    size

--- application/views/Dashboard_V.php
+++ application/views/Dashboard_V.php
@@ -917,7 +917,7 @@
                                     </script>
                                     </head>
                                     <body>
-                                    <div id="chartContainer" style="height: 300px; width: 100%;"></div>
+                                    <div id="chartContainer" style="height: 410px; width: 100%;"></div>
                                     <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
                                     </body>
                                     </html>       
