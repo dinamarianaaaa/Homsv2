f7d58df6501bb03c636bf69b30eb0d4eed0ab7a1
kiddowin <officiallyanakayah@gmail.com>
2019-04-10 16:45:28 +0700

    demo without login

--- application/controllers/Dashboard1_C.php
+++ application/controllers/Dashboard1_C.php
@@ -3,18 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard1_C extends CI_Controller {
 
-	public function __construct(){
+	// public function __construct(){
 		
-		parent::__construct();
+	// 	parent::__construct();
 		
-		$this->load->helper(array('form', 'url'));
-		$this->load->library('form_validation','session');
+	// 	$this->load->helper(array('form', 'url'));
+	// 	$this->load->library('form_validation','session');
         
-        if($this->session->userdata('logged_in') != TRUE){
-            $url = base_url().'login';
-            redirect($url);
-        }
-	}
+    //     if($this->session->userdata('logged_in') != TRUE){
+    //         $url = base_url().'login';
+    //         redirect($url);
+    //     }
+	// }
 	public function index()
 	{
 		$this->load->view('Dashboard1_V');
--- application/controllers/Login_C.php
+++ application/controllers/Login_C.php
@@ -6,55 +6,55 @@ class Login_C extends CI_Controller {
 	{
 		parent::__construct();		
 		$this->load->model('M_Users');
-		$this->load->library('form_validation','session');
+		// $this->load->library('form_validation','session');
 
 	}
 	public function index()
 	{
 		$this->load->view('Login_V');
 	}
-	public function loginAction()
-	{
-		$data['err_message'] = "";
-		$this->form_validation->set_rules('email', 'Email', 'required|valid_email');
-		$this->form_validation->set_rules('pass', 'Password', 'required');
-		//check form validation
-		if ($this->form_validation->run() == FALSE){
-			$data['err_message'] = "";
-		}
-		else{
-			$password = sha1(md5($this->input->post('pass'))); //encrypt the password using sha1 and md5
-			//$password = $this->input->post('pass');
-			$where = array(
-				'email' => $this->input->post('email'),
-				'password' => $password,
-				'status' => 1
-			);
-			$cek = $this->M_Users->cek_login("users",$where)->num_rows();
-			if($cek < 1){
-				$data['err_message'] = "Email/Password doesn't match";
-			}else{
-				//cek if aktif?
-				$whereAktif = array(
-					'email' => $this->input->post('email'),
-					'password' => $password,
-					'is_aktif' => 1
-				);
-				$aktif = $this->M_Users->cek_login("users",$whereAktif)->num_rows();
-				if($aktif < 1){
-					$data['err_message'] = "Your Account is Inactive. Please check your email ". $this->input->post('email');
-				}else{
-					$user 	= $this->M_Users->getUser($this->input->post('email'));
-					$data 		= array(
-	                			'sess_id' => 	$user->id,
-	                			'sess_role' => 	$user->email,
-				        	'logged_in' => 	TRUE
-					);
-					$this->session->set_userdata($data);
-	       				redirect(base_url().'demo');
-				}
-			}
-		}
+	// public function loginAction()
+	// {
+	// 	$data['err_message'] = "";
+	// 	$this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+	// 	$this->form_validation->set_rules('pass', 'Password', 'required');
+	// 	//check form validation
+	// 	if ($this->form_validation->run() == FALSE){
+	// 		$data['err_message'] = "";
+	// 	}
+	// 	else{
+	// 		$password = sha1(md5($this->input->post('pass'))); //encrypt the password using sha1 and md5
+	// 		//$password = $this->input->post('pass');
+	// 		$where = array(
+	// 			'email' => $this->input->post('email'),
+	// 			'password' => $password,
+	// 			'status' => 1
+	// 		);
+	// 		$cek = $this->M_Users->cek_login("users",$where)->num_rows();
+	// 		if($cek < 1){
+	// 			$data['err_message'] = "Email/Password doesn't match";
+	// 		}else{
+	// 			//cek if aktif?
+	// 			$whereAktif = array(
+	// 				'email' => $this->input->post('email'),
+	// 				'password' => $password,
+	// 				'is_aktif' => 1
+	// 			);
+	// 			$aktif = $this->M_Users->cek_login("users",$whereAktif)->num_rows();
+	// 			if($aktif < 1){
+	// 				$data['err_message'] = "Your Account is Inactive. Please check your email ". $this->input->post('email');
+	// 			}else{
+	// 				$user 	= $this->M_Users->getUser($this->input->post('email'));
+	// 				$data 		= array(
+	//                 			'sess_id' => 	$user->id,
+	//                 			'sess_role' => 	$user->email,
+	// 			        	'logged_in' => 	TRUE
+	// 				);
+	// 				$this->session->set_userdata($data);
+	//        				redirect(base_url().'demo');
+	// 			}
+	// 		}
+	// 	}
 		$this->load->view('Login_V',$data);
 	}
 }
